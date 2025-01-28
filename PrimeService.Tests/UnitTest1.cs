a9300e00e7931f7a0acdc34532b89410ba110819
Gregory Bleiker <gregory.bleiker@zund.com>
2025-01-28 10:01:00 +0100

    push commit

---
+++ PrimeService.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿//using Xunit;
+using PrimeService;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Fact]
+        public void IsPrime_InputIs2_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(2);
+
+            Assert.False(result, "2 should not be prime");
+        }
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
---
+++ PrimeService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+           return false;// throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
